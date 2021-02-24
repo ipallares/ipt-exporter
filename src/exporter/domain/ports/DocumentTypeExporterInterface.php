028855659a06d2e467de52e566bef826f5fc013a
Ivan Pallares Trivino <ivan@ipallares.com>
2021-02-24 14:44:58 +0100

    feat: add interface for document type exporters
    Both Docx and Pdf export services will implement this interface.

---
+++ src/exporter/domain/ports/DocumentTypeExporterInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\exporter\domain\ports;
+
+interface DocumentTypeExporterInterface
+{
+    public function addTable(string $text): string;
+
+    public function addRow(string $text): string;
+
+    public function addCell(string $text): string;
+}
