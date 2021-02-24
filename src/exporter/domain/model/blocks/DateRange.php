42322a061a57f9c5b1fd823760a78f6dd67561e8
Ivan Pallares Trivino <ivan@ipallares.com>
2021-02-24 14:41:12 +0100

    feat: add DateRange value object

---
+++ src/exporter/domain/model/blocks/DateRange.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\exporter\domain\model\blocks;
+
+use App\exporter\domain\Exceptions\IniDateAfterEndDateException;
+use DateTime;
+use Exception;
+
+class DateRange
+{
+    private const DATE_FORMAT = 'd-m-Y';
+
+    private DateTime $iniDate;
+    private DateTime $endDate;
+
+    /**
+     * @throws IniDateAfterEndDateException
+     * @throws Exception
+     */
+    private function __construct(DateTime $iniDate, DateTime $endDate)
+    {
+        if ($endDate->getTimestamp() < $iniDate->getTimestamp()) {
+           throw new IniDateAfterEndDateException();
+        }
+
+        $this->iniDate = $iniDate;
+        $this->endDate = $endDate;
+    }
+
+    /**
+     * @throws Exception
+     */
+    public static function fromTimestamps(int $iniTimestamp, int $endTimestamp): DateRange
+    {
+        return new self(
+            self::getDateTimeFromTimestamp($iniTimestamp),
+            self::getDateTimeFromTimestamp($endTimestamp)
+        );
+    }
+
+    /**
+     * @throws Exception
+     */
+    public static function fromDateTimes(DateTime $iniDate, DateTime $endDate): DateRange
+    {
+        return new self($iniDate, $endDate);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public static function fromStrings(string $iniTime, string $endTime): DateRange
+    {
+        return new self(new DateTime($iniTime), new DateTime($endTime));
+    }
+
+    public function export()
+    {
+        return '<daterange>' . $this->formatDateTime($this->iniDate) .  ' - '
+                    . $this->formatDateTime($this->endDate) . '</daterange>';
+    }
+
+    private static function getDateTimeFromTimestamp(int $timestamp): DateTime
+    {
+        $dateTime = new DateTime();
+        $dateTime->setTimestamp($timestamp);
+
+        return $dateTime;
+    }
+
+    private function formatDateTime(DateTime $dateTime): string
+    {
+        return $dateTime->format(self::DATE_FORMAT);
+    }
+}
