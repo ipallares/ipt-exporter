94da3a2f0b8a3719918f7328f7b9fe5788f21cb0
Ivan Pallares Trivino <ivan@ipallares.com>
2021-02-24 14:42:59 +0100

    feat: add CV value object

---
+++ src/exporter/domain/model/CV.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\exporter\domain\model;
+
+use App\exporter\domain\model\sections\AcademicExperiences;
+use App\exporter\domain\model\sections\CoverLetter;
+use App\exporter\domain\model\sections\PersonalDetails;
+use App\exporter\domain\model\sections\WorkExperiences;
+
+class CV
+{
+    private PersonalDetails $personalDetails;
+
+    private CoverLetter $coverLetter;
+
+    private WorkExperiences $workExperiences;
+
+    private AcademicExperiences $academicExperience;
+
+    public function __construct(PersonalDetails $personalDetails, CoverLetter $coverLetter, WorkExperiences $workExperiences, AcademicExperiences $academicExperience)
+    {
+        $this->personalDetails = $personalDetails;
+        $this->coverLetter = $coverLetter;
+        $this->workExperiences = $workExperiences;
+        $this->academicExperience = $academicExperience;
+    }
+
+    public function exportPersonalDetailsCompressed(): string
+    {
+        return $this->personalDetails->exportCompressed();
+    }
+
+    public function exportWorkExperiencesCompressed(): string
+    {
+        return $this->workExperiences->exportCompressed();
+    }
+
+    public function exportAcademicExperiencesCompressed(): string
+    {
+        return $this->academicExperience->exportCompressed();
+    }
+
+    public function exportPersonalDetailsExtended(): string
+    {
+        return $this->personalDetails->exportExtended();
+    }
+
+    public function exportCoverLetterExtended(): string
+    {
+        return $this->coverLetter->export();
+    }
+
+    public function exportWorkExperiencesExtended(): string
+    {
+        return $this->workExperiences->exportExtended();
+    }
+
+    public function exportAcademicExperiencesExtended(): string
+    {
+        return $this->academicExperience->exportExtended();
+    }
+
+}
+// IPT: TODO: BilderPattern to instantiate this object
+
+
