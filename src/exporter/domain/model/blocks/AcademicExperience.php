b98d406ea9e3860f4fe4c53cdb4f32fc6e826794
Ivan Pallares Trivino <ivan@ipallares.com>
2021-02-24 14:40:07 +0100

    feat: add AcademicExperience value object

---
+++ src/exporter/domain/model/blocks/AcademicExperience.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\exporter\domain\model\blocks;
+
+use App\exporter\domain\model\interfaces\MultipleContentLengthExportInterface;
+
+class AcademicExperience implements MultipleContentLengthExportInterface
+{
+    private string $schoolName;
+    private string $title;
+    private DateRange $dateRange;
+    private string $additionalInfo;
+
+    public function __construct(string $schoolName, string $title, DateRange $dateRange, string $additionalInfo = '')
+    {
+        $this->schoolName = $schoolName;
+        $this->title = $title;
+        $this->dateRange = $dateRange;
+        $this->additionalInfo = $additionalInfo;
+    }
+
+    public function exportCompressed(): string
+    {
+        return '<compressed-academicexperience>'
+            . $this->exportDateRange()
+            . $this->exportSchoolName()
+            . $this->exportTitle()
+            . '</compressed-academicexperience>';
+    }
+
+    public function exportExtended(): string
+    {
+        return '<extended-academicexperience>'
+            . $this->exportDateRange()
+            . $this->exportSchoolName()
+            . $this->exportTitle()
+            . $this->exportAdditionalInfo()
+            . "</extended-academicexperience>";
+    }
+
+    private function exportSchoolName(): string
+    {
+        return "<schoolname>$this->schoolName</schoolname>";
+    }
+
+    private function exportTitle(): string
+    {
+        return "<title>$this->title</title>";
+    }
+
+    private function exportDateRange(): string
+    {
+        return $this->dateRange->export();
+    }
+
+    private function exportAdditionalInfo(): string
+    {
+        return "<additionalinfo>$this->additionalInfo</additionalinfo>";
+    }
+}
