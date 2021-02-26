cc2b0671b22b34360b70dcff96451a252ec3c182
Ivan Pallares Trivino <ivan@ipallares.com>
2021-02-26 14:35:35 +0100

    refactor: use files with expected test results

--- tests/exporter/application/services/exporter/LayoutAcademicFirstExporterTest.php
+++ tests/exporter/application/services/exporter/LayoutAcademicFirstExporterTest.php
@@ -16,6 +16,7 @@ class LayoutAcademicFirstExporterTest extends KernelTestCase
 {
     private LayoutAcademicFirstExporter $exporter;
     private JsonToCvConverter $converter;
+    private string $inputJson;
 
     public function setUp(): void
     {
@@ -23,13 +24,13 @@ class LayoutAcademicFirstExporterTest extends KernelTestCase
         self::bootKernel();
         $this->exporter = self::$kernel->getContainer()->get(LayoutAcademicFirstExporter::class);
         $this->converter = self::$kernel->getContainer()->get(JsonToCvConverter::class);
+        $this->inputJson = $this->getInputJson();
     }
 
-    public function testExport_Docx_Compressed_AcademicFirst(): void
+    public function testExport_Docx_Compressed(): void
     {
-        $expected = "<docx-content><recruiterheader>My Awesome Recruitment Company</recruiterheader></docx-content><docx-content><compressed-personaldetails><name>Max</name><surname>Mustermann</surname></compressed-personaldetails></docx-content><docx-content><recruiterbanner></recruiterbanner></docx-content><docx-content><compressed-academicexperiences><compressed-academicexperience><daterange>01-09-2008 - 30-06-2014</daterange><schoolname>The IT University</schoolname><title>Computer Engineer</title></compressed-academicexperience><compressed-academicexperience><daterange>01-09-2006 - 30-06-2008</daterange><schoolname>The IT Institute</schoolname><title>Technical Software Developer</title></compressed-academicexperience><compressed-academicexperiences></docx-content><docx-content><compressed-workexperiences><compressed-workexperience><daterange>15-11-2016 - 31-05-2018</daterange><companyname>My Awesome Company</companyname><position>Senior developer</position></compressed-workexperience><compressed-workexperience><daterange>01-08-2014 - 01-10-2018</daterange><companyname>Another Cool Company</companyname><position>Senior - intermediate developer</position></compressed-workexperience><compressed-workexperiences></docx-content>";
-        $inputJson = file_get_contents('tests/exporter/application/services/converter/inputJsonSchemaV1/input-data/cv-v1.json');
-        $cv = $this->converter->convert($inputJson);
+        $expected = $this->getExpected_Docx_Compressed();
+        $cv = $this->converter->convert($this->inputJson);
         $exported = $this->exporter->export(
             $cv,
             DocxExporter::SUPPORTED_DOCUMENT_TYPE,
@@ -38,11 +39,10 @@ class LayoutAcademicFirstExporterTest extends KernelTestCase
         $this->assertEquals($expected, $exported);
     }
 
-    public function testExport_Docx_Extended_AcademicFirst(): void
+    public function testExport_Docx_Extended(): void
     {
-        $expected = "<docx-content><recruiterheader>My Awesome Recruitment Company</recruiterheader></docx-content><docx-content><extended-personaldetails><name>Max</name><surname>Mustermann</surname><email>max@mustermann.com</email><phonenumber>555-55-55-55</phonenumber></extended-personaldetails></docx-content><docx-content><coverletter>Lorem ipsum dolor sit amet...</coverletter></docx-content><docx-content><recruiterbanner></recruiterbanner></docx-content><docx-content><extended-academicexperiences><extended-academicexperience><daterange>01-09-2008 - 30-06-2014</daterange><schoolname>The IT University</schoolname><title>Computer Engineer</title><additionalinfo>Final Career Project on Machine Learning</additionalinfo></extended-academicexperience><extended-academicexperience><daterange>01-09-2006 - 30-06-2008</daterange><schoolname>The IT Institute</schoolname><title>Technical Software Developer</title><additionalinfo>6 months internship at 'Another Cool Company'</additionalinfo></extended-academicexperience><extended-academicexperiences></docx-content><docx-content><extended-workexperiences><extended-workexperience><daterange>15-11-2016 - 31-05-2018</daterange><companyname>My Awesome Company</companyname><position>Senior developer</position><taskdescription>Taking care of refactoring the project to clean code and clean architectures.</taskdescription></extended-workexperience><extended-workexperience><daterange>01-08-2014 - 01-10-2018</daterange><companyname>Another Cool Company</companyname><position>Senior - intermediate developer</position><taskdescription>Collaborating with IT Lead to design the project.</taskdescription></extended-workexperience><extended-workexperiences></docx-content>";
-        $inputJson = file_get_contents('tests/exporter/application/services/converter/inputJsonSchemaV1/input-data/cv-v1.json');
-        $cv = $this->converter->convert($inputJson);
+        $expected = $this->getExpected_Docx_Extended();
+        $cv = $this->converter->convert($this->inputJson);
         $exported = $this->exporter->export(
             $cv,
             DocxExporter::SUPPORTED_DOCUMENT_TYPE,
@@ -51,11 +51,10 @@ class LayoutAcademicFirstExporterTest extends KernelTestCase
         $this->assertEquals($expected, $exported);
     }
 
-    public function testExport_Pdf_Compressed_AcademicFirst(): void
+    public function testExport_Pdf_Compressed(): void
     {
-        $expected = "<pdf-content><recruiterheader>My Awesome Recruitment Company</recruiterheader></pdf-content><pdf-content><compressed-personaldetails><name>Max</name><surname>Mustermann</surname></compressed-personaldetails></pdf-content><pdf-content><recruiterbanner></recruiterbanner></pdf-content><pdf-content><compressed-academicexperiences><compressed-academicexperience><daterange>01-09-2008 - 30-06-2014</daterange><schoolname>The IT University</schoolname><title>Computer Engineer</title></compressed-academicexperience><compressed-academicexperience><daterange>01-09-2006 - 30-06-2008</daterange><schoolname>The IT Institute</schoolname><title>Technical Software Developer</title></compressed-academicexperience><compressed-academicexperiences></pdf-content><pdf-content><compressed-workexperiences><compressed-workexperience><daterange>15-11-2016 - 31-05-2018</daterange><companyname>My Awesome Company</companyname><position>Senior developer</position></compressed-workexperience><compressed-workexperience><daterange>01-08-2014 - 01-10-2018</daterange><companyname>Another Cool Company</companyname><position>Senior - intermediate developer</position></compressed-workexperience><compressed-workexperiences></pdf-content>";
-        $inputJson = file_get_contents('tests/exporter/application/services/converter/inputJsonSchemaV1/input-data/cv-v1.json');
-        $cv = $this->converter->convert($inputJson);
+        $expected = $this->getExpected_Pdf_Compressed();
+        $cv = $this->converter->convert($this->inputJson);
         $exported = $this->exporter->export(
             $cv,
             PdfExporter::SUPPORTED_DOCUMENT_TYPE,
@@ -64,16 +63,54 @@ class LayoutAcademicFirstExporterTest extends KernelTestCase
         $this->assertEquals($expected, $exported);
     }
 
-    public function testExport_Pdf_Extended_AcademicFirst(): void
+    public function testExport_Pdf_Extended(): void
     {
-        $expected = "<pdf-content><recruiterheader>My Awesome Recruitment Company</recruiterheader></pdf-content><pdf-content><extended-personaldetails><name>Max</name><surname>Mustermann</surname><email>max@mustermann.com</email><phonenumber>555-55-55-55</phonenumber></extended-personaldetails></pdf-content><pdf-content><coverletter>Lorem ipsum dolor sit amet...</coverletter></pdf-content><pdf-content><recruiterbanner></recruiterbanner></pdf-content><pdf-content><extended-academicexperiences><extended-academicexperience><daterange>01-09-2008 - 30-06-2014</daterange><schoolname>The IT University</schoolname><title>Computer Engineer</title><additionalinfo>Final Career Project on Machine Learning</additionalinfo></extended-academicexperience><extended-academicexperience><daterange>01-09-2006 - 30-06-2008</daterange><schoolname>The IT Institute</schoolname><title>Technical Software Developer</title><additionalinfo>6 months internship at 'Another Cool Company'</additionalinfo></extended-academicexperience><extended-academicexperiences></pdf-content><pdf-content><extended-workexperiences><extended-workexperience><daterange>15-11-2016 - 31-05-2018</daterange><companyname>My Awesome Company</companyname><position>Senior developer</position><taskdescription>Taking care of refactoring the project to clean code and clean architectures.</taskdescription></extended-workexperience><extended-workexperience><daterange>01-08-2014 - 01-10-2018</daterange><companyname>Another Cool Company</companyname><position>Senior - intermediate developer</position><taskdescription>Collaborating with IT Lead to design the project.</taskdescription></extended-workexperience><extended-workexperiences></pdf-content>";
-        $inputJson = file_get_contents('tests/exporter/application/services/converter/inputJsonSchemaV1/input-data/cv-v1.json');
-        $cv = $this->converter->convert($inputJson);
+        $expected = $this->getExpected_Pdf_Extended();
+        $cv = $this->converter->convert($this->inputJson);
         $exported = $this->exporter->export(
             $cv,
             PdfExporter::SUPPORTED_DOCUMENT_TYPE,
             ExtendedExporter::CONTENT_LENGTH_TYPE_EXTENDED
         );
         $this->assertEquals($expected, $exported);
+    }
+
+
+    private function getExpected_Docx_Compressed(): string
+    {
+        $expected = file_get_contents('tests/exporter/application/services/exporter/expected-documents/academic-first-compressed-docx.xml');
+
+        return $this->cleanString($expected);
+    }
+
+    private function getExpected_Docx_Extended(): string
+    {
+        $expected = file_get_contents('tests/exporter/application/services/exporter/expected-documents/academic-first-extended-docx.xml');
+
+        return $this->cleanString($expected);
+    }
+
+    private function getExpected_Pdf_Compressed(): string
+    {
+        $expected = file_get_contents('tests/exporter/application/services/exporter/expected-documents/academic-first-compressed-pdf.xml');
+
+        return $this->cleanString($expected);
+    }
+
+    private function getExpected_Pdf_Extended(): string
+    {
+        $expected = file_get_contents('tests/exporter/application/services/exporter/expected-documents/academic-first-extended-pdf.xml');
+
+        return $this->cleanString($expected);
+    }
+
+    private function cleanString(string $text): string
+    {
+        return str_replace(["\t", "\n", "  "], '', $text);
+    }
+
+    private function getInputJson()
+    {
+        return file_get_contents('tests/exporter/application/services/converter/inputJsonSchemaV1/input-data/cv-v1.json');
     }
 }
