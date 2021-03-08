3b485724e7f34c8c2f18e494fd6f42239a0d7da3
Ivan Pallares Trivino <ivan@ipallares.com>
2021-03-08 17:23:29 +0100

    refactor: apply Template Pattern to exporter tests

---
+++ tests/exporter/application/services/exporter/ExporterTest.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\exporter\application\services\exporter;
+
+use App\exporter\application\services\exporter\Exporter;
+use App\exporter\infrastructure\adapters\DocxExporter;
+use App\exporter\infrastructure\adapters\PdfExporter;
+use App\Tests\exporter\application\services\exporter\traits\ExportersTestHelperTrait;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+abstract class ExporterTest extends KernelTestCase
+{
+    use ExportersTestHelperTrait;
+
+    private Exporter $exporter;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        self::bootKernel();
+        $this->exporter = $this->getExporter();
+    }
+
+    public function testWordExport_cv1_v1(): void
+    {
+        $expected = $this->getExpected_Word_cv1_v1();
+
+        $exported = $this->exporter->export(
+            $this->getInputJson_cv1(),
+            DocxExporter::SUPPORTED_DOCUMENT_TYPE,
+            self::$container->getParameter('cv_schema_v1')
+        );
+
+        $this->assertEquals($expected, $this->cleanString($exported));
+    }
+
+    public function testWordExport_cv2_v1(): void
+    {
+        $expected = $this->getExpected_Word_cv2_v1();
+
+        $exported = $this->exporter->export(
+            $this->getInputJson_cv2(),
+            DocxExporter::SUPPORTED_DOCUMENT_TYPE,
+            self::$container->getParameter('cv_schema_v1')
+        );
+
+        $this->assertEquals($expected, $this->cleanString($exported));
+    }
+
+    public function testPdfExport_cv1_v1(): void
+    {
+        $expected = $this->getExpected_Pdf_cv1_v1();
+
+        $exported = $this->exporter->export(
+            $this->getInputJson_cv1(),
+            PdfExporter::SUPPORTED_DOCUMENT_TYPE,
+            self::$container->getParameter('cv_schema_v1')
+        );
+
+        $this->assertEquals($expected, $this->cleanString($exported));
+    }
+
+    public function testPdfExport_cv2_v1(): void
+    {
+        $expected = $this->getExpected_Pdf_cv2_v1();
+
+        $exported = $this->exporter->export(
+            $this->getInputJson_cv2(),
+            PdfExporter::SUPPORTED_DOCUMENT_TYPE,
+            self::$container->getParameter('cv_schema_v1')
+        );
+
+        $this->assertEquals($expected, $this->cleanString($exported));
+    }
+
+    abstract protected function getExporter();
+}
--- tests/exporter/application/services/exporter/ExporterWithRecursiveArrayIteratorTest.php
+++ tests/exporter/application/services/exporter/ExporterWithRecursiveArrayIteratorTest.php
@@ -5,73 +5,11 @@ declare(strict_types=1);
 namespace App\Tests\exporter\application\services\exporter;
 
 use App\exporter\application\services\exporter\ExporterWithRecursiveArrayIterator;
-use App\exporter\infrastructure\adapters\DocxExporter;
-use App\exporter\infrastructure\adapters\PdfExporter;
-use App\Tests\exporter\application\services\exporter\traits\ExportersTestHelperTrait;
-use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
-class ExporterWithRecursiveArrayIteratorTest extends KernelTestCase
+class ExporterWithRecursiveArrayIteratorTest extends ExporterTest
 {
-    use ExportersTestHelperTrait;
-
-    private ExporterWithRecursiveArrayIterator $exporter;
-
-    public function setUp(): void
+    protected function getExporter()
     {
-        parent::setUp();
-        self::bootKernel();
-        $this->exporter = self::$container->get(ExporterWithRecursiveArrayIterator::class);
-    }
-
-    public function testWordExport_cv1_v1(): void
-    {
-        $expected = $this->getExpected_Word_cv1_v1();
-
-        $exported = $this->exporter->export(
-            $this->getInputJson_cv1(),
-            DocxExporter::SUPPORTED_DOCUMENT_TYPE,
-            self::$container->getParameter('cv_schema_v1')
-        );
-
-        $this->assertEquals($expected, $this->cleanString($exported));
-    }
-
-    public function testWordExport_cv2_v1(): void
-    {
-        $expected = $this->getExpected_Word_cv2_v1();
-
-        $exported = $this->exporter->export(
-            $this->getInputJson_cv2(),
-            DocxExporter::SUPPORTED_DOCUMENT_TYPE,
-            self::$container->getParameter('cv_schema_v1')
-        );
-
-        $this->assertEquals($expected, $this->cleanString($exported));
-    }
-
-    public function testPdfExport_cv1_v1(): void
-    {
-        $expected = $this->getExpected_Pdf_cv1_v1();
-
-        $exported = $this->exporter->export(
-            $this->getInputJson_cv1(),
-            PdfExporter::SUPPORTED_DOCUMENT_TYPE,
-            self::$container->getParameter('cv_schema_v1')
-        );
-
-        $this->assertEquals($expected, $this->cleanString($exported));
-    }
-
-    public function testPdfExport_cv2_v1(): void
-    {
-        $expected = $this->getExpected_Pdf_cv2_v1();
-
-        $exported = $this->exporter->export(
-            $this->getInputJson_cv2(),
-            PdfExporter::SUPPORTED_DOCUMENT_TYPE,
-            self::$container->getParameter('cv_schema_v1')
-        );
-
-        $this->assertEquals($expected, $this->cleanString($exported));
+        return self::$container->get(ExporterWithRecursiveArrayIterator::class);
     }
 }
