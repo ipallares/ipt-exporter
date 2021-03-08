d7ce7f8ad4d3461a96a0999193f3ad64766789f7
Ivan Pallares Trivino <ivan@ipallares.com>
2021-03-08 11:27:46 +0100

    feat: add trait for test common methods

---
+++ tests/exporter/application/services/exporter/traits/ExporterHelperTrait.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Tests\exporter\application\services\exporter\traits;
+
+trait ExporterHelperTrait
+{
+    private function getInputJson_cv1(): string
+    {
+        return file_get_contents('tests/exporter/application/services/exporter/input-data/cv1-v1.json');
+    }
+
+    private function getInputJson_cv2(): string
+    {
+        return file_get_contents('tests/exporter/application/services/exporter/input-data/cv2-v1.json');
+    }
+
+    private function getInputJsonArray_cv1(): object
+    {
+        return json_decode($this->getInputJson_cv1());
+    }
+
+    private function getExpected_Word_cv1_v1(): string
+    {
+        $expected = file_get_contents('tests/exporter/application/services/exporter/expected-documents/recursive-iterator-iterator/docx-cv1-v1.xml');
+
+        return $this->cleanString($expected);
+    }
+
+    private function getExpected_Word_cv2_v1(): string
+    {
+        $expected = file_get_contents('tests/exporter/application/services/exporter/expected-documents/recursive-iterator-iterator/docx-cv2-v1.xml');
+
+        return $this->cleanString($expected);
+    }
+
+    private function getExpected_Pdf_cv1_v1(): string
+    {
+        $expected = file_get_contents('tests/exporter/application/services/exporter/expected-documents/recursive-iterator-iterator/pdf-cv1-v1.xml');
+
+        return $this->cleanString($expected);
+    }
+
+    private function getExpected_Pdf_cv2_v1(): string
+    {
+        $expected = file_get_contents('tests/exporter/application/services/exporter/expected-documents/recursive-iterator-iterator/pdf-cv2-v1.xml');
+
+        return $this->cleanString($expected);
+    }
+
+    private function cleanString(string $text): string
+    {
+        return str_replace(["\t", "\n", "\r", "  "], '', $text);
+    }
+}
