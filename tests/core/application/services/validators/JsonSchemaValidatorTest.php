5de7e7c443396a1c456292f95f79713699ab767e
Ivan Pallares Trivino <ivan@ipallares.com>
2021-03-08 11:28:20 +0100

    refactor: adapt JsonSchemaValidatorTest

--- tests/core/application/services/validators/JsonSchemaValidatorTest.php
+++ tests/core/application/services/validators/JsonSchemaValidatorTest.php
@@ -5,12 +5,15 @@ declare(strict_types=1);
 namespace App\Tests\core\application\services\validators;
 
 use App\core\application\services\validators\JsonSchemaValidator;
+use App\Tests\exporter\application\services\exporter\traits\ExporterHelperTrait;
 use Exception;
 use JsonSchema\Exception\InvalidSchemaException;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class JsonSchemaValidatorTest extends KernelTestCase
 {
+    use ExporterHelperTrait;
+
     private string $inputJson;
 
     private string $schemaPath;
@@ -23,18 +26,20 @@ class JsonSchemaValidatorTest extends KernelTestCase
         self::bootKernel();
         $this->validator = self::$kernel->getContainer()->get(JsonSchemaValidator::class);
 
-        $this->inputJson = file_get_contents(
-            'tests/exporter/application/services/exporter/input-data/cv1-v1.json'
-        );
-
         $this->schemaPath = self::$kernel->getContainer()->getParameter('cv_schema_v1');
     }
 
-    public function testValidJson(): void
+    public function testValidJsons(): void
+    {
+        $this->assertValidJson($this->getInputJson_cv1());
+        $this->assertValidJson($this->getInputJson_cv2());
+    }
+
+    private function assertValidJson(string $json): void
     {
         try{
             $exceptionThrown = false;
-            $this->validator->validate($this->inputJson, $this->schemaPath);
+            $this->validator->validate($json, $this->schemaPath);
         } catch (Exception $e) {
             $exceptionThrown = true;
         }
@@ -44,7 +49,7 @@ class JsonSchemaValidatorTest extends KernelTestCase
 
     public function testInvalidJson_MissingPersonalDetails(): void
     {
-        $jsonObject = json_decode($this->inputJson);
+        $jsonObject = $this->getInputJsonArray_cv1();
         unset($jsonObject->personalDetails);
         $invalidJson = json_encode($jsonObject);
         $this->expectException(InvalidSchemaException::class);
@@ -58,7 +63,7 @@ class JsonSchemaValidatorTest extends KernelTestCase
 
     public function testInvalidJson_MissingCoverLetter(): void
     {
-        $jsonObject = json_decode($this->inputJson);
+        $jsonObject = $this->getInputJsonArray_cv1();
         unset($jsonObject->coverLetter);
         $invalidJson = json_encode($jsonObject);
         $this->expectException(InvalidSchemaException::class);
@@ -73,7 +78,7 @@ class JsonSchemaValidatorTest extends KernelTestCase
 
     public function testInvalidJson_MissingWorkExperiences(): void
     {
-        $jsonObject = json_decode($this->inputJson);
+        $jsonObject = $this->getInputJsonArray_cv1();
         unset($jsonObject->workExperiences);
         $invalidJson = json_encode($jsonObject);
         $this->expectException(InvalidSchemaException::class);
@@ -88,7 +93,7 @@ class JsonSchemaValidatorTest extends KernelTestCase
 
     public function testInvalidJson_MissingAcademicExperiences(): void
     {
-        $jsonObject = json_decode($this->inputJson);
+        $jsonObject = $this->getInputJsonArray_cv1();
         unset($jsonObject->academicExperiences);
         $invalidJson = json_encode($jsonObject);
         $this->expectException(InvalidSchemaException::class);
