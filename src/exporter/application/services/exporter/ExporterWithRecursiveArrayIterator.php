772c4949a3c59eb5f77aedd9870bb65fbe0ccd53
Ivan Pallares Trivino <ivan@ipallares.com>
2021-03-08 17:15:16 +0100

    refactor: adapt specific Export services to use the Template

--- src/exporter/application/services/exporter/ExporterWithRecursiveArrayIterator.php
+++ src/exporter/application/services/exporter/ExporterWithRecursiveArrayIterator.php
@@ -4,66 +4,15 @@ declare(strict_types=1);
 
 namespace App\exporter\application\services\exporter;
 
-use App\core\application\services\validators\JsonSchemaValidator;
-use App\exporter\application\services\exporter\traits\ExportersHelperTrait;
 use App\exporter\domain\ports\DocumentTypeExporterInterface;
-use App\exporter\domain\services\DocumentTypeExporterFinder;
 use Iterator;
 use RecursiveArrayIterator;
 use RecursiveIteratorIterator;
 
-class ExporterWithRecursiveArrayIterator
+class ExporterWithRecursiveArrayIterator extends Exporter
 {
-    use ExportersHelperTrait;
 
-    private DocumentTypeExporterFinder $documentTypeExporterFinder;
-    private JsonSchemaValidator $schemaValidator;
-    private string $cvSchemaV1;
-
-    public function __construct(
-        DocumentTypeExporterFinder $documentTypeExporterFinder,
-        JsonSchemaValidator $schemaValidator,
-        string $cvSchemaV1
-    )
-    {
-        $this->documentTypeExporterFinder = $documentTypeExporterFinder;
-        $this->schemaValidator = $schemaValidator;
-        $this->cvSchemaV1 = $cvSchemaV1;
-    }
-
-    public function export(string $json, string $documentType, string $cvSchemaV1): string
-    {
-        // IPT: TODO: Check if here some pattern to chain calls for validation and getDocumentTypExporter
-        $this->schemaValidator->validate($json, $cvSchemaV1);
-        // Get the proper Strategy by passing the expected documentType and a 'supports' method implemented by every Strategy.
-        $documentTypeExporter = $this->documentTypeExporterFinder->find($documentType);
-
-        $iterator = new RecursiveArrayIterator(
-            json_decode($json, true),
-            RecursiveIteratorIterator::SELF_FIRST
-        );
-
-        return $this->doExport($documentTypeExporter, $iterator);
-    }
-
-    private function doExport(
-        DocumentTypeExporterInterface $documentTypeExporter,
-        RecursiveArrayIterator $iterator,
-        int $depth = 0
-    ): string {
-
-        $exportedText = '';
-        foreach ($iterator as $tag => $item) {
-            //$exportedText .= $this->exportPart($documentTypeExporter, $iterator, $key, $item);
-            $exportedText .= $this->isCompoundItem($item)
-                ? $this->getCompoundItemContent($documentTypeExporter, $iterator, $tag, $depth)
-                : $this->getSingleItemContent($documentTypeExporter, $item, $tag);
-        }
-
-        return $exportedText;
-    }
-
-    private function isCompoundItem($item, $tag = ''): bool
+    protected function isCompoundItem($item, $tag = ''): bool
     {
         return is_array($item);
     }
@@ -71,11 +20,11 @@ class ExporterWithRecursiveArrayIterator
     /**
      * @param int | string $tag
      */
-    private function getCompoundItemContent(
+    protected function getCompoundItemContent(
         DocumentTypeExporterInterface $documentTypeExporter,
         Iterator $iterator,
         $tag,
-        int $depth
+        int $depth = 0
     ): string {
 
         $result = '';
@@ -87,15 +36,11 @@ class ExporterWithRecursiveArrayIterator
         return  $result . $this->doExport($documentTypeExporter, $iterator->getChildren(), ++$depth);
     }
 
-    /**
-     * @param array | string $item
-     * @param int | string $tag
-     */
-    private function getSingleItemContent(DocumentTypeExporterInterface $documentTypeExporter, $item, $tag): string {
-        $openTag = $this->openTag($tag);
-        $content = $this->getItemStringContent($item);
-        $closeTag = $this->closeTag($tag);
-
-        return $documentTypeExporter->addContent($openTag . $content . $closeTag);
+    protected function getIterator(string $json): Iterator
+    {
+        return new RecursiveArrayIterator(
+            json_decode($json, true),
+            RecursiveIteratorIterator::SELF_FIRST
+        );
     }
 }
--- src/exporter/application/services/exporter/ExporterWithRecursiveIteratorIterator.php
+++ src/exporter/application/services/exporter/ExporterWithRecursiveIteratorIterator.php
@@ -4,63 +4,15 @@ declare(strict_types=1);
 
 namespace App\exporter\application\services\exporter;
 
-use App\core\application\services\validators\JsonSchemaValidator;
-use App\exporter\application\services\exporter\traits\ExportersHelperTrait;
 use App\exporter\domain\ports\DocumentTypeExporterInterface;
-use App\exporter\domain\services\DocumentTypeExporterFinder;
+use Iterator;
 use RecursiveArrayIterator;
 use RecursiveIteratorIterator;
 
-class ExporterWithRecursiveIteratorIterator
+class ExporterWithRecursiveIteratorIterator extends Exporter
 {
-    use ExportersHelperTrait;
 
-    private DocumentTypeExporterFinder $documentTypeExporterFinder;
-    private JsonSchemaValidator $schemaValidator;
-    private string $cvSchemaV1;
-
-    public function __construct(
-        DocumentTypeExporterFinder $documentTypeExporterFinder,
-        JsonSchemaValidator $schemaValidator,
-        string $cvSchemaV1
-    )
-    {
-        $this->documentTypeExporterFinder = $documentTypeExporterFinder;
-        $this->schemaValidator = $schemaValidator;
-        $this->cvSchemaV1 = $cvSchemaV1;
-    }
-
-    public function export(string $json, string $documentType, string $cvSchemaV1): string
-    {
-        // IPT: TODO: Check if here some pattern to chain calls for validation and getDocumentTypExporter
-        $this->schemaValidator->validate($json, $cvSchemaV1);
-        // Get the proper Strategy by passing the expected documentType and a 'supports' method implemented by every Strategy.
-        $documentTypeExporter = $this->documentTypeExporterFinder->find($documentType);
-
-        $iterator = new RecursiveIteratorIterator(
-            new RecursiveArrayIterator(json_decode($json, true)),
-            RecursiveIteratorIterator::SELF_FIRST
-        );
-
-        return $this->doExport($documentTypeExporter, $iterator);
-    }
-
-    private function doExport(
-        DocumentTypeExporterInterface $documentTypeExporter,
-        RecursiveIteratorIterator $iterator
-    ): string {
-
-        $exportedText = '';
-        foreach ($iterator as $tag => $item) {
-            $exportedText .= $this->isCompoundItem($item, $tag)
-                ? $this->getCompoundItemContent($documentTypeExporter, $iterator, $tag)
-                : $this->getSingleItemContent($documentTypeExporter, $item, $tag);
-        }
-
-        return $exportedText;
-    }
-
-    private function isCompoundItem($item, $tag): bool
+    protected function isCompoundItem($item, $tag = ''): bool
     {
         return !is_string($item) && $this->isValidTag($tag);
     }
@@ -68,10 +20,11 @@ class ExporterWithRecursiveIteratorIterator
     /**
      * @param int | string $tag
      */
-    private function getCompoundItemContent(
+    protected function getCompoundItemContent(
         DocumentTypeExporterInterface $documentTypeExporter,
-        RecursiveIteratorIterator $iterator,
-        $tag
+        Iterator $iterator,
+        $tag,
+        int $depth = 0
     ): string {
 
         $headerTag = 0 === $iterator->getDepth() ? 'section' : 'block';
@@ -79,17 +32,11 @@ class ExporterWithRecursiveIteratorIterator
         return  $documentTypeExporter->addContent("<$headerTag>$tag</$headerTag>");
     }
 
-    /**
-     * @param array | string $item
-     * @param int | string $tag
-     */
-    private function getSingleItemContent(DocumentTypeExporterInterface $documentTypeExporter, $item, $tag): string {
-        $openTag = $this->openTag($tag);
-        $content = $this->getItemStringContent($item);
-        $closeTag = $this->closeTag($tag);
-
-        return $documentTypeExporter->addContent($openTag . $content . $closeTag);
+    protected function getIterator(string $json): Iterator
+    {
+        return new RecursiveIteratorIterator(
+            new RecursiveArrayIterator(json_decode($json, true)),
+            RecursiveIteratorIterator::SELF_FIRST
+        );
     }
-
-
 }
